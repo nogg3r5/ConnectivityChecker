cebc5973a61845e65674c960c577b43d68022a3d
Shaun Johnson <nogg3r5@gmail.com>
2022-02-06 21:06:03 +0000

    Functional, but only works if the site is up

--- ConnectivityChecker.py
+++ ConnectivityChecker.py
@@ -1,9 +1,23 @@
 import requests
-import json
+import sys
 
-def main(sites):
+#to be replaced with argparse
+args = sys.argv[1:]
+
+def checkArgs():
+ if not args:
+  sys.exit('please pass at least one url as an argument')
+
+def check(sites):
+ checkArgs()
  list = []
- foreach $site in $sites:
-  res = requests.get($site)
+ for site in sites:
+  site = 'http://' + site
+  res = requests.get(site)
   list.append(res)
- return list
+ print(list)
+
+#First imte i've used this, runs the script only if being called from cli. __name__ only == __main__ when run from cli
+#When imorted, main is still available, but will not be executed.
+if __name__ == "__main__":
+    check(args)
