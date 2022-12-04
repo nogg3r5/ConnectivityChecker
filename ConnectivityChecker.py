7b5bef7e69218fdd801370d555481afc2576cdd2
Nogg3r5 <nogg3r5@gmail.com>
2022-12-04 13:02:00 +0000

    Adding script to run the checks and a socket tests for non-http connections

--- ConnectivityChecker.py
+++ ConnectivityChecker.py
@@ -1,23 +1,36 @@
 import requests
 import sys
-import sqlite
+import socket
+import sqlite3
 
 #to be replaced with argparse
-args = sys.argv[1:]
+#args = sys.argv[1:]
 
 def checkArgs():
  if not args:
   sys.exit('please pass at least one url as an argument')
 
 def check(sites):
- checkArgs()
+ #checkArgs()
  list = []
  for site in sites:
   site = 'http://' + site
+  print(site)
   res = requests.get(site)
   list.append(res)
+  print(list)
  print(list)
 
+def checkSocket(sites):
+ for site in sites:
+  conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  try:
+   conn.connect((site, 80))
+   print('success')
+  except:
+   print('Failed') 
+   return
+  return
 #First imte i've used this, runs the script only if being called from cli. __name__ only == __main__ when run from cli
 #When imorted, main is still available, but will not be executed.
 if __name__ == "__main__":
---
+++ runner.py
@@ -0,0 +1,6 @@
+import ConnectivityChecker as cc
+
+hosts = ['192.168.4.48','192.168.4.48']
+sockets =['192.168.4.34']
+cc.check(hosts)
+cc.checkSocket(sockets)
