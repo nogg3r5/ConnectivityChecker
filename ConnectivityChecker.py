07728d5d10517e700a789ab758e3988bfb1ffa83
Nogg3r5 <nogg3r5@gmail.com>
2023-01-21 21:06:33 +0000

    Put sockets back in the runner and added SQL writes to failures, ensured that last up and last down dates are retained.

--- ConnectivityChecker.py
+++ ConnectivityChecker.py
@@ -10,8 +10,7 @@ def check(sites):
   addr = 'http://' + site
   type='site'
   try:
-   print(addr)
-   requests.get(addr)
+   requests.get(addr,timeout = 5)
   except:
    IsUp=False
    writeToDb(site,type,IsUp)
@@ -27,14 +26,16 @@ def checkSocket(sites):
  list=[]
  for site in sites:
   conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  type='socket'
   try:
    conn.connect((site, 80))
-   req=True
-   writeToDb(site,type,IsUp)
   except:
-   req=False
-   writeToDb(False)
- res=[req,site]
+   IsUp=False
+   writeToDb(site,type,IsUp)
+  else:
+   IsUp=True
+   writeToDb(site,type,IsUp)
+ res=[IsUp,site]
  list.append(res)
  print(list)
 
@@ -44,21 +45,27 @@ def writeToDb(site,type,IsUp):
 
  if(IsUp == True):
   lastUp = datetime.now()
-  print("Is up would be written todb")
-  print(site,type,lastUp,IsUp)
-  cursor.execute("INSERT or replace INTO ConnectivityCheck (id,type,lastUp,IsUp) values(?, ?, ?, ?)",(site,type,lastUp,IsUp))
+  query = f"""select * from ConnectivityCheck where id = '{site}'; """
+  cursor.execute(query)
+  data = cursor.fetchall()
+  lastDown=data[0][4]
+  cursor.execute("INSERT or replace INTO ConnectivityCheck (id,type,lastUp,lastDown,IsUp) values(?, ?, ?, ?,?)",(site,type,lastUp,lastDown,IsUp))
   sqliteConnection.commit()
   cursor.close()
   sqliteConnection.close()
 
  else:
-  print("Is down would be wrtten to DB")
-  #lastdown=date
-  #lastUp=get from db
+  query = f"""select * from ConnectivityCheck where id = '{site}'; """
+  cursor.execute(query)
+  data = cursor.fetchall()
+  lastUp=data[0][3]
+  lastDown = datetime.now()
+  cursor.execute("INSERT or replace INTO ConnectivityCheck (id,type,lastUp,lastDown,IsUp) values(?, ?, ?, ?, ?)",(site,type,lastUp,lastDown,IsUp))
+  sqliteConnection.commit()
+  cursor.close()
+  sqliteConnection.close()
 
-
-
-#CREATE TABLE ConnectivityCheck (id integer primary key autoincrement,site TEXT,type TEXT,lastcheck DATETIME default current_timestamp,lastUp datetime,lastDown datetime,IsUp integer);
+#CREATE TABLE ConnectivityCheck (id text primary key autoincrement,type TEXT,lastcheck DATETIME default current_timestamp,lastUp datetime,lastDown datetime,IsUp integer);
 
 
 if __name__ == "__main__":
--- runner.py
+++ runner.py
@@ -1,6 +1,10 @@
 import ConnectivityChecker as cc
 
-hosts = ['192.168.4.48','google.com']
-sockets =['192.168.4.34','192.168.4.48']
+hosts = ['192.168.4.48','nogg3r5.hopto.org']
+sockets =['192.168.4.34']
+
+print('Conventional hosts...')
 cc.check(hosts)
-#cc.checkSocket(sockets)
+print('')
+print('Sockets...')
+cc.checkSocket(sockets)
