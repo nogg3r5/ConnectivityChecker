3e64455717be279c6e879fe6b3acf2465e5b7ea5
Nogg3r5 <nogg3r5@gmail.com>
2024-12-11 21:22:25 +0000

    added google.co.uk to list of sites, so we can be sure that we can reach the internet.

--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 import ConnectivityChecker as cc
 
-hosts =['pizero2.tail06b125.ts.net:5000','raspberrypi4b.tail06b125.ts.net:3000/dashboard']
+hosts =['pizero2.tail06b125.ts.net:5000','raspberrypi4b.tail06b125.ts.net:3000/dashboard','google.co.uk']
 sockets =['192.168.4.34']
 services =['PersonalDashboard']
 
